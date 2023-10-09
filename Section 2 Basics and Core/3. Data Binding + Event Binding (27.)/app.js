e8abef9ba1c7a19cdb912c90f85846c600ce3466
niczyji <niczyji@gmail.com>
2023-10-09 14:06:29 +0200

    Feature: add function outputFullname

--- Section 2 Basics and Core/3. Data Binding + Event Binding (27.)/app.js
+++ Section 2 Basics and Core/3. Data Binding + Event Binding (27.)/app.js
@@ -6,6 +6,13 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    outputFullname() {
+      console.log("Count");
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Kowalski";
+    },
     setName(event, lastName) {
       this.name = event.target.value;
     },
