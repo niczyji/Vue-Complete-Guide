a81e9bffefdec60fefcf4cba66a2aed23d96dfb1
niczyji <niczyji@gmail.com>
2023-10-11 12:53:18 +0200

    Feature: add 3. Assignment, Chore: change naming

--- Section 2 Basics and Core/3. Data Binding + Event Binding (27.)/app.js
+++ Section 2 Basics and Core/3. Data Binding + Event Binding (27.)/app.js
@@ -8,6 +8,11 @@ const app = Vue.createApp({
     };
   },
   watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    },
     /*     name(value) {
       if (value === "") {
         this.fullname = "";
