d68b433051ecbaacbe166656a7a7036d7750a00c
niczyji <niczyji@gmail.com>
2023-11-02 20:31:51 +0100

    Feature: add classes

--- Section 2 Basics and Core/Assignments/3. Assignment Dynamic Styling (36.)/app.js
+++ Section 2 Basics and Core/Assignments/3. Assignment Dynamic Styling (36.)/app.js
@@ -2,13 +2,25 @@ const app = Vue.createApp({
   data() {
     return {
       inputClass: "",
+      isVisible: true,
     };
   },
   methods: {
+    toggleClasses() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+  computed: {
     appliedClasses() {
       return {
         user1: this.inputClass === "user1",
         user2: this.inputClass === "user2",
+      };
+    },
+    visibleClass() {
+      return {
+        visible: this.isVisible,
+        hidden: !this.isVisible,
       };
     },
   },
