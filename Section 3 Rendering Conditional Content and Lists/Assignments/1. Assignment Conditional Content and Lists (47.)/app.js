278a18da4640965bc1ca70b6f98f44f11cd9dc30
unknown <niczyji>
2023-12-02 20:35:08 +0100

    Add method toggleButton to hide/show ul

--- Section 3 Rendering Conditional Content and Lists/Assignments/1. Assignment Conditional Content and Lists (47.)/app.js
+++ Section 3 Rendering Conditional Content and Lists/Assignments/1. Assignment Conditional Content and Lists (47.)/app.js
@@ -3,12 +3,22 @@ const app = Vue.createApp({
       return {
         tasks: [],
         inputTask: "",
+        isActive: false,
       };
     },
     methods: {
      addTask() {
       this.tasks.push(this.inputTask)
       console.log(this.tasks);
+     },
+     toggleButton() {
+      if (!this.isActive) {
+        this.isActive = true;
+        console.log("Active");
+      } else {
+        this.isActive = false;
+        console.log("Not Active");
+      }
      }
     },
   });
