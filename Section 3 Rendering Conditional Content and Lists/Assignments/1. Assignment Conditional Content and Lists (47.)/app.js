950c98e36366bdbe7d3519a02f998b9fbb3297bb
niczyji <niczyji@gmail.com>
2023-12-05 21:51:06 +0100

    Update toggleButton function aka clean code

--- Section 3 Rendering Conditional Content and Lists/Assignments/1. Assignment Conditional Content and Lists (47.)/app.js
+++ Section 3 Rendering Conditional Content and Lists/Assignments/1. Assignment Conditional Content and Lists (47.)/app.js
@@ -1,27 +1,20 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        tasks: [],
-        inputTask: "",
-        isActive: false,
-      };
-    },
-    methods: {
-     addTask() {
-      this.tasks.push(this.inputTask)
+  data() {
+    return {
+      tasks: [],
+      inputTask: "",
+      isActive: false,
+    };
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.inputTask);
       console.log(this.tasks);
-     },
-     toggleButton() {
-      if (!this.isActive) {
-        this.isActive = true;
-        console.log("Active");
-      } else {
-        this.isActive = false;
-        console.log("Not Active");
-      }
-     }
     },
-  });
-  
-  app.mount("#assignment");
-  
+    toggleButton() {
+      this.isActive = !this.isActive;
+    },
+  },
+});
+
+app.mount("#assignment");
