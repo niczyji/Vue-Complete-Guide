0bdfb455e4c5bcdc8b5cab16d252293bbcb31773
unknown <niczyji>
2023-12-06 10:25:50 +0100

    Add computed functions to update Health Bars

--- Section 4 Course Project The Monster Slayer Game/app.js
+++ Section 4 Course Project The Monster Slayer Game/app.js
@@ -1,22 +1,31 @@
 const app = Vue.createApp({
   data() {
     return {
-      playerHP: 200,
       monsterHP: 200,
+      playerHP: 200,
     };
   },
   methods: {
     attackButton() {
-        damagePlayerHP = Math.floor(Math.random() * 21);
-        damageMonsterHP = Math.floor(Math.random() * 21);
-        console.log(damagePlayerHP, damageMonsterHP);
-        this.playerHP = this.playerHP - damagePlayerHP;
-        this.monsterHP =  this.monsterHP - damageMonsterHP;
-        console.log("Player HP:", this.playerHP, "Monster HP:", this.monsterHP);    },
+      let damageMonsterHP = Math.floor(Math.random() * 21);
+      let damagePlayerHP = Math.floor(Math.random() * 21);
+      console.log(
+        "Damage to Player:",
+        damagePlayerHP,
+        "Damage to Monster:",
+        damageMonsterHP
+      );
+      this.monsterHP = this.monsterHP - damageMonsterHP;
+      this.playerHP = this.playerHP - damagePlayerHP;
+      console.log("Player HP:", this.playerHP, "Monster HP:", this.monsterHP);
+    },
   },
   computed: {
-    updateHealthBars() {
-
+    monsterHealthBarWidth() {
+       return this.monsterHP < 1 ? "0%" : (this.monsterHP / 200) * 100 + "%"
+    },
+    playerHealthBarWidth() {
+        return this.playerHP < 1 ? "0%" : (this.playerHP / 200) * 100 + "%"
     },
   },
 });
