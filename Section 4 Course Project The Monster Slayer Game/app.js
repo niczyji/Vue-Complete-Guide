fdcc01dcd4e7382ed0fdaadd73b0c517ce841e5c
unknown <niczyji>
2023-12-06 12:02:39 +0100

    Add specialAttackButton and healButton

--- Section 4 Course Project The Monster Slayer Game/app.js
+++ Section 4 Course Project The Monster Slayer Game/app.js
@@ -3,21 +3,33 @@ const app = Vue.createApp({
     return {
       monsterHP: 200,
       playerHP: 200,
+      battleLog: [],
+      disabledSpecialAttack: true,
     };
   },
   methods: {
     attackButton() {
       let damageMonsterHP = Math.floor(Math.random() * 21);
       let damagePlayerHP = Math.floor(Math.random() * 21);
-      console.log(
-        "Damage to Player:",
-        damagePlayerHP,
-        "Damage to Monster:",
-        damageMonsterHP
-      );
+  
+      this.battleLog.push(`Monster attacks and deals ${damagePlayerHP}`);
+      this.battleLog.push(`Player attacks and deals ${damageMonsterHP}`);
+  
       this.monsterHP = this.monsterHP - damageMonsterHP;
       this.playerHP = this.playerHP - damagePlayerHP;
-      console.log("Player HP:", this.playerHP, "Monster HP:", this.monsterHP);
+    },
+    specialAttackButton() {
+        let damageMonsterHP = Math.floor(Math.random() * 21);
+        let damagePlayerHP = Math.floor(Math.random() * 41);
+        this.battleLog.push(`Monster attacks and deals ${damagePlayerHP}`);
+        this.battleLog.push(`Player attacks and deals ${damageMonsterHP}`);
+        this.disabledSpecialAttack = !this.disabledSpecialAttack;
+    },
+    healButton() {
+      let healAmount = Math.floor(Math.random() * 21);
+      this.playerHP = this.playerHP + healAmount > 200 ? 200 : this.playerHP + healAmount;
+      this.battleLog.push(`Player heals for ${healAmount}`);
+      this.disabledSpecialAttack = !this.disabledSpecialAttack;
     },
   },
   computed: {
