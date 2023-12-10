b67d571d565921dc0430c9a91a6442d977542104
niczyji <niczyji@gmail.com>
2023-12-10 16:31:43 +0100

    Clean a code

--- Section 4 Course Project The Monster Slayer Game/app.js
+++ Section 4 Course Project The Monster Slayer Game/app.js
@@ -1,3 +1,5 @@
+const MAX_HP = 200;
+
 function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -5,8 +7,8 @@ function getRandomValue(min, max) {
 const app = Vue.createApp({
   data() {
     return {
-      monsterHP: 200,
-      playerHP: 200,
+      monsterHP: MAX_HP,
+      playerHP: MAX_HP,
       currentRound: 0,
       battleLog: [],
     };
@@ -36,16 +38,20 @@ const app = Vue.createApp({
       this.battleLog.push(`Player attacks and deals ${damageMonsterHP}`);
     },
     healPlayerButton() {
+      this.currentRound++;
+
       let healAmount = getRandomValue(15, 25);
 
       this.playerHP =
-        this.playerHP + healAmount > 200 ? 200 : this.playerHP + healAmount;
+        this.playerHP + healAmount > MAX_HP
+          ? MAX_HP
+          : this.playerHP + healAmount;
 
       this.battleLog.push(`Player heals for ${healAmount}`);
     },
     startNewGame() {
-      this.monsterHP = 200;
-      this.playerHP = 200;
+      this.monsterHP = MAX_HP;
+      this.playerHP = MAX_HP;
       this.battleLog = [];
     },
   },
@@ -60,17 +66,14 @@ const app = Vue.createApp({
       return this.currentRound % 3 !== 0;
     },
     monsterHealthBarWidth() {
-      return this.monsterHP < 1 ? "0" : (this.monsterHP / 200) * 100;
+      return this.monsterHP < 1 ? "0" : (this.monsterHP / MAX_HP) * 100;
     },
     playerHealthBarWidth() {
-      return this.playerHP < 1 ? "0" : (this.playerHP / 200) * 100;
+      return this.playerHP < 1 ? "0" : (this.playerHP / MAX_HP) * 100;
     },
     winCondition() {
-      if (this.monsterHP <= 0) {
-        return "Player Wins!";
-      } else if (this.playerHP <= 0) {
-        return "Monster Wins!";
-      }
+      if (this.monsterHP <= 0) return "Player Wins!";
+      if (this.playerHP <= 0) return "Monster Wins!";
       return "";
     },
   },
