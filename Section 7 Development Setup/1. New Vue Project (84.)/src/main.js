38332b7754576cbf7bbdda97140ab00a7c1817c4
niczyji <niczyji@gmail.com>
2023-12-21 21:25:10 +0100

    Add test app

--- Section 7 Development Setup/1. New Vue Project (84.)/src/main.js
+++ Section 7 Development Setup/1. New Vue Project (84.)/src/main.js
@@ -1,3 +1,20 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
 
-createApp({}).mount('');
+// Definiere eine einfache Komponente
+const App = {
+  template: `
+    <div id="app">
+      <h1>Hello Vue!</h1>
+      <button @click="count++">Klick mich</button>
+      <p>Du hast {{ count }} mal geklickt.</p>
+    </div>
+  `,
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+
+// Initialisiere und mounte die Vue-App
+createApp(App).mount("#app");
