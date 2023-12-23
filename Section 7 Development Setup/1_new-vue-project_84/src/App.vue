9f30a97ce2247f0801a5f7084b209a4d38cd4ef8
niczyji <niczyji@gmail.com>
2023-12-23 12:54:31 +0100

    Add New vue project, add new naming

---
+++ Section 7 Development Setup/1_new-vue-project_84/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Hello, Vue!</h1>
+    <p>Welcome to your Vue.js app!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
