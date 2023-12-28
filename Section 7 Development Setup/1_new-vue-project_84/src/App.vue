89d71e16e4f21e872faa2dd85a204abb43e080f2
niczyji <niczyji@gmail.com>
2023-12-28 11:36:07 +0100

    Update App.vue, add v-for and friends list

--- Section 7 Development Setup/1_new-vue-project_84/src/App.vue
+++ Section 7 Development Setup/1_new-vue-project_84/src/App.vue
@@ -1,14 +1,50 @@
 <template>
-  <div id="app">
-    <h1>Hello, Vue!</h1>
-  </div>
+  <h1>My Friends</h1>
+  <ul>
+    <li v-for="(friend, index) in friends" :key="index">
+      Hallo {{ friend.name }}
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: "App",
   data() {
-    return {};
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Luise",
+          phone: "12412344214",
+          email: "manuel@example.com",
+        },
+        {
+          id: "sophia",
+          name: "Sophia Martinez",
+          phone: "3125550198",
+          email: "sophia.martinez@example.com",
+        },
+        {
+          id: "alex",
+          name: "Alex Johnson",
+          phone: "4155550269",
+          email: "alex.johnson@example.com",
+        },
+        {
+          id: "lisa",
+          name: "Lisa Chang",
+          phone: "3235550731",
+          email: "lisa.chang@example.com",
+        },
+        {
+          id: "omar",
+          name: "Omar Hassan",
+          phone: "2135550812",
+          email: "omar.hassan@example.com",
+        },
+      ],
+    };
   },
   methods: {},
 };
