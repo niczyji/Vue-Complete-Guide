8644883b227fa5bcd85098d0e265fd7eeeab9179
niczyji <niczyji@gmail.com>
2024-01-02 11:29:15 +0100

    Add workaround to update props

--- Section 8 Component Communication/1_starting-setup_91/src/components/FriendContact.vue
+++ Section 8 Component Communication/1_starting-setup_91/src/components/FriendContact.vue
@@ -1,6 +1,7 @@
 <template>
   <li>
-    <h2>{{ name }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : '' }}</h2>
+    <button @click="toggleDetails">Toggle Favorite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
       <li>
@@ -20,7 +21,8 @@ export default {
   props: [
     'name',
     'phoneNumber',
-    'emailAddress'
+    'emailAddress',
+    'isFavorite',
   ],
   data() {
     return {
@@ -31,11 +33,19 @@ export default {
         phone: "0123 45678 90",
         email: "manuel@localhost.com",
       },
+      friendIsFavorite: this.isFavorite,
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      if (this.friendIsFavorite === '1') {
+        friendIsFavorite = '0'      
+      } else {
+        friendIsFavorite = '1'      
+      }
     }
   }
 };
