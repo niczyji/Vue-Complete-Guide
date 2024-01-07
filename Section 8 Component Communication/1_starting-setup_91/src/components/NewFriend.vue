90a80557f53bcca29edb9d321fe81be7f89e9468
niczyji <niczyji@gmail.com>
2024-01-07 20:01:23 +0100

    Register 'NewFriend' Component in Main Application File

--- Section 8 Component Communication/1_starting-setup_91/src/components/NewFriend.vue
+++ Section 8 Component Communication/1_starting-setup_91/src/components/NewFriend.vue
@@ -1,5 +1,21 @@
 <template lang="">
-  <div></div>
+  <form action="">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for="phone">Phone</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for="email">E-mail</label>
+      <input type="text" />
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
 </template>
 <script>
 export default {};
