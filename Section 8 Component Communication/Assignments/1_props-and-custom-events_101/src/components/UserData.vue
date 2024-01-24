0899c1840c18241d0f1c5a0f298e86a9735843d7
niczyji <niczyji@gmail.com>
2024-01-24 15:08:03 +0100

    Add Style

--- Section 8 Component Communication/Assignments/1_props-and-custom-events_101/src/components/UserData.vue
+++ Section 8 Component Communication/Assignments/1_props-and-custom-events_101/src/components/UserData.vue
@@ -1,6 +1,16 @@
 <template lang="">
   <div>
     <h2>UserData</h2>
+    <form @submit.prevent="submitData">
+      <div>
+        <label>Name</label>
+        <input type="name" v-model="enteredName" />
+      </div>
+      <div>
+        <label>Age</label>
+        <input type="age" v-model="enteredAge" />
+      </div>
+    </form>
   </div>
 </template>
 <script>
